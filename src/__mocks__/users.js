a2278550b6b135abc47b0e5152a11bee3832e88d
Daniel Grisolia <daniel.grisolia@stormsec.com.br>
2020-01-07 15:47:23 -0300

    criacao do mock de usuarios

---
+++ src/__mocks__/users.js
@@ -0,0 +1,52 @@
+const users = {
+  data: [
+    {
+      user: 'ANPINA',
+      email: 'antonio.pina@tvglobo.com.br',
+      date_inclusion: '28/05/2019',
+      date_change: '30/05/2019',
+      rules: '01',
+      status: 'ATIVO',
+    },
+    {
+      user: 'CCHANG',
+      email: 'ciro.chang@tvglobo.com.br',
+      date_inclusion: '28/05/2019',
+      date_change: '30/05/2019',
+      rules: '01',
+      status: 'ATIVO',
+    },
+    {
+      user: 'TMARCAL',
+      email: 'thiago.marcal@tvglobo.com.br',
+      date_inclusion: '28/05/2019',
+      date_change: '30/05/2019',
+      rules: '01',
+      status: 'ATIVO',
+    },
+    {
+      user: 'ECGIANN',
+      email: 'ecgiannotto@tvglobo.com.br',
+      date_inclusion: '28/05/2019',
+      date_change: '30/05/2019',
+      rules: '01',
+      status: 'ATIVO',
+    },
+    {
+      user: 'YFERNAND',
+      email: 'yuri.vasquez@tvglobo.com.br',
+      date_inclusion: '28/05/2019',
+      date_change: '30/05/2019',
+      rules: '02',
+      status: 'ATIVO',
+    },
+    {
+      user: 'PLACERDA',
+      email: 'pedro.soares.lacerda@tvglobo.com.br',
+      date_inclusion: '28/05/2019',
+      date_change: '30/05/2019',
+      rules: '02',
+      status: 'ATIVO',
+    },
+  ],
+};
